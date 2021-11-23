3fdf6220606ff6550778333d44152530ed1ac492
Yigit Kemal Erinc <yigit.erinc@tum.de>
2021-11-23 22:53:16 +0100

    Fix folder structure

---
+++ src/main/java/com/group5/customerauthenticationservice/CustomerAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.group5.customerauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerAuthenticationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group5/customerauthenticationservice/CustomerAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.customerauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
