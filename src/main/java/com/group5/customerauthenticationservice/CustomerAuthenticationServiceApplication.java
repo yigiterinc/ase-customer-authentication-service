352df28a2b51e65f323613b38f13ddbe4b704ba2
Yigit Kemal Erinc <erincyigit@gmail.com>
2022-01-23 14:11:14 +0100

    Add Eureka client for service discovery

--- src/main/java/com/group5/customerauthenticationservice/CustomerAuthenticationServiceApplication.java
+++ src/main/java/com/group5/customerauthenticationservice/CustomerAuthenticationServiceApplication.java
@@ -1,13 +1,11 @@
 package com.group5.customerauthenticationservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerAuthenticationServiceApplication {
 
     public static void main(String[] args) {
